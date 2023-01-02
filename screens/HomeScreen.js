8c22e2f7a89909863b77e11454fd10c3b9edf5a3
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-02 04:35:33 -0800

    home design and animation it and all complete and button set

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import React, { useLayoutEffect } from 'react';
+import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 import { HeroImage } from '../assets';
 
@@ -45,15 +46,17 @@ const HomeScreen = () => {
         {/* Image container */}
 
         <View className="flex-1 relative items-center justify-center">
-            <Image
+            <Animatable.Image
+                animation="fadeIn"
+                easing="ease-in-out"
                 source={HeroImage} className="w-full h-full object-cover mt-20"
             />
 
             <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center">
                 <TouchableOpacity>
-                    <View className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
+                    <Animatable.View animation={"pulse"} easing="ease-in-out" iterationCount={"infinite"} className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
                     <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
-                </View>
+                </Animatable.View>
                 </TouchableOpacity>
             </View>
         </View>
