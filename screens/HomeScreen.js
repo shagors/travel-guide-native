a8fbf22179f6a91125fbcee1efb17d576c3f0d9d
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-02 03:00:50 -0800

    Design home menu and body section and check it

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown : false,
+
+        });
+    }, [])
+
   return (
-    <View>
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 relative">
+        {/* first section */}
+
+        <View className="flex-row px-6 mt-8 space-x-2">
+            <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+                <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+            </View>
+            <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+        </View>
+
+        {/* Second section */}
+
+        <View className="px-6 mt-8 space-y-3">
+            <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+            <Text className="text-[#00BCC9] text-[38px] font-bold">Goods Moments</Text>
+
+            <Text className="text-[#3C6072] text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
+                quis nostrum
+            </Text>
+        </View>
+
+        {/* circle Section */}
+
+        <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-36 -right-36"></View>
+        <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36"></View>
+
+        {/* Image container */}
+
+    </SafeAreaView>
   )
 }
 
