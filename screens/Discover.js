80ca2df1e7f8c7d171534cedf2ba93ff08a0d5c4
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-02 23:26:42 -0800

    design discover page and add images and set logo and mark it and condition apply

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,15 +1,18 @@
-import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import React, { useLayoutEffect, useState } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { useNavigation } from '@react-navigation/native';
 import { Attractions, Avatar, Hotels, Restaurants } from '../assets';
 import MenuContainer from '../components/MenuContainer';
+import { FontAwesome } from '@expo/vector-icons';
+import ItemCardContainer from '../components/ItemCardContainer';
 
 const Discover = () => {
 
   const navigation = useNavigation();
 
   const [type, setType] = useState("restaurants");
+  const [isLoading, setIsLoading] = useState(false);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -43,40 +46,72 @@ const Discover = () => {
             console.log( details?.geometry?.viewport);
           }}
           query={{
-            key: 'your api key',
+            key: 'api key',
             language: 'en',
           }}
         />
       </View>
 
       {/* Menu Container */}
-        
-      <ScrollView>
-        <View className="flex-row items-center justify-between px-8 mt-8">
-          <MenuContainer 
-            key={"hotels"}
-            title="Hotels"
-            imageSrc={Hotels}
-            type={type}
-            setType={setType}
-          />
-          <MenuContainer
-            key={"attractions"}
-            title="Attractions"
-            imageSrc={Attractions}
-            type={type}
-            setType={setType}
-          />
-
-          <MenuContainer
-            key={"restaurants"}
-            title="Restaurants"
-            imageSrc={Restaurants}
-            type={type}
-            setType={setType}
-          />
+      {isLoading ? (
+        <View>
+          <View className=" flex-1 items-center justify-center">
+            <ActivityIndicator size="large" color="#0B646B" />
+          </View>
         </View>
-      </ScrollView>
+        ) : 
+
+        (<ScrollView>
+          <View className="flex-row items-center justify-between px-8 mt-8">
+            <MenuContainer
+              key={"hotels"}
+              title="Hotels"
+              imageSrc={Hotels}
+              type={type}
+              setType={setType}
+            />
+            <MenuContainer
+              key={"attractions"}
+              title="Attractions"
+              imageSrc={Attractions}
+              type={type}
+              setType={setType}
+            />
+
+            <MenuContainer
+              key={"restaurants"}
+              title="Restaurants"
+              imageSrc={Restaurants}
+              type={type}
+              setType={setType}
+            />
+          </View>
+
+          <View>
+            <View className="flex-row items-center justify-between px-4 mt-8">
+              <Text className="text-[#2C7379] text-[28px] font-bold">
+                Top Tips
+              </Text>
+              <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+                <Text className="text-[#A0C4C7] text-[20px] font-bold">
+                  Explore
+                </Text>
+                <FontAwesome
+                  name="long-arrow-right"
+                  size={24}
+                  color="#A0C4C7"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+              <ItemCardContainer key={"101"} imageSrc={"https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpghttps://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"} title="Something" location="Rangpur" />
+              <ItemCardContainer key={"102"} imageSrc={"https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"} title="Anything" location="Doha" />
+            </View>
+          </View>
+        </ScrollView>)
+      }
+      
 
     </SafeAreaView>
   )
